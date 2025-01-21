1e1dbf335bbbd067064abf026b54870cec43b53b
Yurii Dubnytskyi <yur.dub7@gmail.com>
2025-01-21 20:51:14 +0200

    create app

--- App.tsx
+++ App.tsx
@@ -1,20 +1,63 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useCallback, useState } from "react";
+import {
+  Button,
+  StyleSheet,
+  Text,
+  TouchableWithoutFeedback,
+  View,
+} from "react-native";
 
-export default function App() {
+const App = () => {
+  const [background, setBackground] = useState("#FFFFFF");
+
+  const generateRandomColor = useCallback(() => {
+    const randomColor =
+      "#" +
+      Math.floor(Math.random() * 16777215)
+        .toString(16)
+        .padStart(6, "0");
+    setBackground(randomColor);
+  }, []);
+
+  const resetColor = useCallback(() => {
+    setBackground("#FFFFFF");
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <TouchableWithoutFeedback onPress={generateRandomColor}>
+      <View style={[styles.wrapper, { backgroundColor: background }]}>
+        <View style={styles.container}>
+          <Text>Hello there</Text>
+          <Text>Background is {background}</Text>
+          <View style={styles.button}>
+            <Button title="Reset" onPress={resetColor} color={"purple"} />
+          </View>
+          <StatusBar style="auto" />
+        </View>
+      </View>
+    </TouchableWithoutFeedback>
   );
-}
+};
+
+export default App;
 
 const styles = StyleSheet.create({
-  container: {
+  wrapper: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
+    display: "flex",
+  },
+  container: {
+    height: 150,
+    width: 150,
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#fff",
+  },
+  button: {
+    marginTop: 10,
   },
 });
---
+++ README.md
@@ -0,0 +1,36 @@
+# Simple App (generate random background color)
+
+- It took 1 hour to develop this application
+
+## App Functionality
+
+- **Change background by typing anywhere in screen**
+- **Reset background to default(white)**
+
+# Screenshots
+
+# Run Project Local
+
+## Step 1: Install dependencies
+
+```bash
+# using npm
+npm install
+
+# OR using Yarn
+yarn install
+```
+
+## Step 2: Start your Application
+
+### For Android
+
+```bash
+npm run android
+```
+
+### For iOS
+
+```bash
+npm run ios
+```
